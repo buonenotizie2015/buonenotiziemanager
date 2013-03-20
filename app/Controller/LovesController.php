08bb52e3981234d43d09566525dee28f5e60c997
ipalo <rosso.ipalo@gmail.com>
2013-03-20 19:19:56 +0100

    debugging add love

--- app/Controller/LovesController.php
+++ app/Controller/LovesController.php
@@ -34,7 +34,7 @@ class LovesController extends AppController {
 					echo "ok";
 				} else {
 					$this->render(false);
-					echo "bnsk: ".$this->request->data['BNsecretkey'];
+					echo "bnsk: ".$this->request->data['Love'];
 					echo "error";
 				}
 			}

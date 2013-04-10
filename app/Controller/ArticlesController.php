cc6f9b40d13779a1e0eb758f3c5d1606b0ac682c
ipalo <rosso.ipalo@gmail.com>
2013-04-10 23:43:05 +0200

    debug stream parameters

--- app/Controller/ArticlesController.php
+++ app/Controller/ArticlesController.php
@@ -72,6 +72,7 @@ class ArticlesController extends AppController {
 				$conditions = array("Category.parent_id" => explode(',',$this->request->data['main_category']));
 		}
 		
+		$this->log($this->request->data['main_category']);
 		$this->log($conditions);
 		
 		$this->Article->unbindModel(array('belongsTo' => array('Category')));
